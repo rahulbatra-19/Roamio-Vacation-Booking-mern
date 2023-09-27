23796601b9f6facf1608552cb657533000656e63
rahulbatra-19 <batrarahul409@gmail.com>
2023-09-27 15:45:24 +0530

    used cookie

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 require("dotenv").config();
 const app = express();
 
 const bcrpytSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "fshfjahbmajakjbkjbkjk";
 
 app.use(express.json());
 app.use(
@@ -43,16 +45,15 @@ app.post("/login", async (req, res) => {
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-      // jwt.sign(
-      //   { email: userDoc.email, id: userDoc._id },
-      //   jwtSecret,
-      //   {},
-      //   (err, token) => {
-      //     if (err) throw err;
-      //     res.cookie("token", token).json(userDoc);
-      //   }
-      // );
-      res.json("Pass ok");
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
     } else {
       res.status(422).json("Pass not ok");
     }
