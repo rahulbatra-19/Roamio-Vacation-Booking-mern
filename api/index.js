451b4216abbb54a00b80fac17916e6b148231d5c
rahulbatra-19 <batrarahul409@gmail.com>
2023-10-21 02:56:51 +0530

    for specific

--- api/index.js
+++ api/index.js
@@ -7,7 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://roamio-app.netlify.app/",
+  })
+);
 // mongoose.connect(process.env.MONGO_URL);
 // using express Routes
 app.use("/api", require("./routes"));
